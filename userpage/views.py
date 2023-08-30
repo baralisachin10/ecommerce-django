fedd403090442e0a280d2be8f6450d1af40da617
sachin barali <sachinbarali41@gmail.com>
2023-08-30 07:37:27 +0545

    homepage modified

--- userpage/views.py
+++ userpage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from products.models import Products
 # Create your views here.
 
 def home_page(request):
-    return render(request,"client/homepage.html")
+    products = Products.objects.all().order_by('-id')[:8]
+    context = {
+        'products': products
+    }
+    return render(request,"client/homepage.html",context)
